d009357dd2e0fdce9385ab29fd86b65702326b48
Marcos Henrique Garcia Pereira <marcoshgpereira@hotmail.com>
2026-01-16 19:39:42 -0300

    copy

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -13,12 +13,7 @@ export default function About() {
           <h2 className="about-title ">Quem somos</h2>
         </div>
         <p className="about-text">
-          O projeto "Lagarto em Movimento: Histórias, Treinos e Vida Saudável" nasce com o propósito de registrar,
-          valorizar e celebrar as práticas esportivas e os hábitos saudáveis presentes no dia a dia da cidade de
-          Lagarto. Através da fotografia, o projeto conta histórias reais de movimento, superação e disciplina,
-          promovendo bem-estar, inclusão e qualidade de vida. Mais do que imagens, a iniciativa busca inspirar pessoas,
-          fortalecer laços comunitários e mostrar o esporte como um caminho acessível para uma vida mais ativa e
-          saudável.
+          O projeto "Lagarto em Movimento: Histórias, Treinos e Vida Saudável", produzido por estudantes do Instituto Federal de Sergipe (IFS), nasce com o propósito de registrar, valorizar e celebrar as práticas esportivas e os hábitos saudáveis presentes no dia a dia da cidade de Lagarto. Através da fotografia, o projeto conta histórias reais de movimento, superação e disciplina, promovendo bem-estar, inclusão e qualidade de vida. Mais do que imagens, a iniciativa busca inspirar pessoas, fortalecer laços comunitários e mostrar o esporte como um caminho acessível para uma vida mais ativa e saudável.
         </p>
       </div>
     </section>
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -11,6 +11,8 @@ export default function Hero() {
           MOVIMENTO
         </h1>
         <p className="hero-subtitle">Histórias, Treinos e Vida Saudável</p>
+        <p className="hero-subtitle">Lagarto é uma cidade localizada no centro-sul do estado de Sergipe que, nos últimos anos, tem apresentado um aumento significativo na prática de esportes. Esse crescimento está relacionado à maior valorização da saúde, do bem-estar e da convivência comunitária, impulsionada por iniciativas locais que incentivam hábitos de vida mais ativos e saudáveis.</p>
+
       </div>
     </section>
   )
